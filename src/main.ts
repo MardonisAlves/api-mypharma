dad8755afcc9b82e39d4fd5c206510c7500156c7
= <=>
2023-03-31 20:22:25 -0300

    swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ProductModule } from './productmodule/product.module';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 async function bootstrap() {
   const app = await NestFactory.create(ProductModule);
+  app.useGlobalPipes(new ValidationPipe());
+  
+  const config = new DocumentBuilder()
+    .setTitle('API MYPHARMA')
+    .setDescription('api list products')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/doc', app, document);
+
 
   await app.listen(process.env.PORT);
 
